518a7811c70e729cbec2cdba2f5feadcf4c09d26
zulaigahbenjamin <130163047+zulaigahbenjamin@users.noreply.github.com>
2023-08-30 16:10:23 +0200

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,23 +2,28 @@ import mysql2 from "mysql2";
 import dotenv from 'dotenv';
 dotenv.config();
 
-// create the connection to database
-
-const db = mysql2.createConnection({
-        host     : process.env.host,
-        database : process.env.database,
-        user     : process.env.user,
-        password : process.env.password,
-  multipleStatements: true,
-  connectionLimit:30,
+// Create the connection pool
+const pool = mysql2.createPool({
+    host: process.env.host,
+    database: process.env.database,
+    user: process.env.user,
+    password: process.env.password,
+    multipleStatements: true,
+    connectionLimit: 30,
 });
 
-db.connect((err) => {
+// Test the connection
+pool.getConnection((err, connection) => {
     if (err) {
-      console.error('Error connecting to MySQL:', err);
-      return;
+        console.error('Error connecting to MySQL:', err);
+        return;
     }
+    
     console.log('Connected to MySQL server');
-  });
 
-export default db;
+    // Release the connection back to the pool
+    connection.release();
+});
+
+// Export the connection pool
+export default pool;
