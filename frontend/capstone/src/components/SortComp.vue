0c2835b7c779069a4b16826e8c1ec1d2d78f4402
zulaigahbenjamin <130163047+zulaigahbenjamin@users.noreply.github.com>
2023-09-07 11:22:52 +0200

    search and price low to high fixed

--- frontend/capstone/src/components/SortComp.vue
+++ frontend/capstone/src/components/SortComp.vue
@@ -1,5 +1,44 @@
 <template>
- 
+  <div class="col-md-12 mb-4 input-group">
+    <input
+      class="form-control search"
+      placeholder="What are you looking for?"
+      @keyup="search($event.target.value)"
+    />
+    <div class="input-group-append">
+      <div class="sort-filter-container">
+        <div class="filter-container">
+          <label for="filter">Filter by:</label>
+          <select id="filter" v-model="selectedCategory">
+            <option value="all">All Items</option>
+            <option value="Lightly Roasted">Lightly Roasted</option>
+            <option value="Arabica">Arabica</option>
+            <option value="Lightly Roasted">Lightly Roasted</option>
+          </select>
+          <label style="margin-left: 20px"
+            >&#11820; | Showing 1-0 of 10 results</label
+          >
+        </div>
+        <div class="sort-container">
+          <label>Show </label>
+          <label
+            style="
+              background-color: white;
+              color: rgb(58, 58, 58);
+              padding: 7px;
+            "
+          >
+            16</label
+          >
+          <label for="sort">Sort by:</label>
+          <select id="sort" v-model="sortOrder">
+            <option value="price-high">Price Low to High</option>
+            <option value="price-low">Price High to Low</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <div class="container-fluid">
     <div class="row gap-3 justify-content-center" v-if="products">
@@ -27,8 +66,7 @@
         </div>
       </div>
     </div>
-   
-
+  
    
   </div>
 </template>
@@ -36,81 +74,87 @@
 <script>
 export default {
   name: "products",
+  props: ["products", "filters"],
+  emits: ["set-filters"],
+  setup(props, context) {
+    const search = (s) => {
+      const filtered = props.products.filter((product) =>
+        product.prodName.toLowerCase().includes(s.toLowerCase())
+      );
+      context.emit("set-filters", {
+        ...props.filters,
+        s,
+        page: 1,
+      });
+    };
 
-  // emits: ["set-filters"],
-  // setup(props, context) {
-  //   const search = (s) => {
-  //     const filtered = props.products.filter((product) =>
-  //       product.prodName.toLowerCase().includes(s.toLowerCase())
-  //     );
-  //     context.emit("set-filters", {
-  //       ...props.filters,
-  //       s,
-  //       page: 1,
-  //     });
-  //   };
+    const sort = (sort) => {
+      context.emit("set-filters", {
+        ...props.filters,
+        sort,
+        page: 1,
+      });
+    };
 
-    // const sort = (sort) => {
-    //   context.emit("set-filters", {
-    //     ...props.filters,
-    //     sort,
-    //     page: 1,
-    //   });
-    // };
-
-  //   return {
-  //     search,
-  //     sort,
-  //   };
-  // },
-
-  // methods: {
-  //   filter() {
-  //     this.filteredProducts = this.products.filter((product) => {
-  //       if (product.category == this.selectedCategory) {
-  //         return this.selectedCategory;
-  //       }
-  //     });
-  //   },
-  //   sortProducts() {
-  //     if (this.sortOrder === "price-high") {
-  //       this.filteredProducts = this.products.sort(
-  //         (a, b) => a.amount - b.amount
-  //       );
-  //     } else if (this.sortOrder === "price-low") {
-  //       this.filteredProducts = this.products.sort(
-  //         (a, b) => b.amount - a.amount
-  //       );
-  //     }
-  //   },
-  //   sortPrice() {
-  //     this.$store.commit("sortPropertiesByPrice");
-  //   },
-  // },
-  // watch: {
-  //   selectedCategory: "filter",
-  //   sortOrder: "sortProducts",
-  // },
-  // computed: {
-
-  //   properties() {
-  //     return this.$store.state.properties?.filter((property) => {
-  //       let isMatch = true;
-  //       if (
-  //         !property.title.toLowerCase().includes(this.search.toLowerCase())
-  //       ) {
-  //         isMatch = false;
-  //       }
-  //       if (this.area !== "All" && this.area !== property.area) {
-  //         isMatch = false;
-  //       }
-  //       return isMatch;
-  //     });
-  //   },
-  // },
-  mounted() {
-    this.$store.dispatch("getProducts");
+    return {
+      search,
+      sort,
+    };
   },
+  data() {
+    return {
+      product: null,
+      selectedProduct: null,
+      sortOrder: "price-high",
+      selectedCategory: "all",
+      categories: ["Arabica", "Lightly Roasted", "Roasted"],
+    };
+  },
+  methods: {
+    filter() {
+      this.filteredProducts = this.products.filter((product) => {
+        if (product.category == this.selectedCategory) {
+          return this.selectedCategory;
+        }
+      });
+    },
+    sortProducts() {
+      if (this.sortOrder === "price-high") {
+        this.filteredProducts = this.products.sort(
+          (a, b) => a.amount - b.amount
+        );
+      } else if (this.sortOrder === "price-low") {
+        this.filteredProducts = this.products.sort(
+          (a, b) => b.amount - a.amount
+        );
+      }
+    },
+    sortPrice() {
+      this.$store.commit("sortPropertiesByPrice");
+    },
+  },
+  watch: {
+    selectedCategory: "filter",
+    sortOrder: "sortProducts",
+  },
+  computed: {
+
+    properties() {
+      return this.$store.state.properties?.filter((property) => {
+        let isMatch = true;
+        if (
+          !property.title.toLowerCase().includes(this.search.toLowerCase())
+        ) {
+          isMatch = false;
+        }
+        if (this.area !== "All" && this.area !== property.area) {
+          isMatch = false;
+        }
+        return isMatch;
+      });
+    },
+  },
+ 
 };
 </script>
 
--- frontend/capstone/src/store/index.js
+++ frontend/capstone/src/store/index.js
@@ -1,226 +1,89 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex';
+
 const apiUrl = 'https://zulaigahcapstoneapi.onrender.com/';
-import axios from "axios";
-import Cookies from "js-cookie";
+
 export default createStore({
   state: {
-    users: null,
-    user: null,
     products: null,
     product: null,
-    token: null,
-    userData: null,
-    userRole: null,
-    msg: null,
-    error: null,
-    regStatus: null,
-    logStatus: null,
-    cart: [],
+    users: null,
+    user: null,
+    filteredProducts: [],
+    
   },
-
+  getters: {
+  },
   mutations: {
-    setCart(state, cartData) {
-      state.cart = cartData;
-    },
-    removeItemFromCart(state, index) {
-      state.cart.splice(index, 1);
-      localStorage.setItem("cart", JSON.stringify(state.cart));
-    },
-    addItemToCart(state, product) {
-      state.cart.push(product);
-      localStorage.setItem("cart", JSON.stringify(state.cart));
-    },
 
     SET_PRODUCTS(state, products) {
       state.products = products;
     },
+
     SET_PRODUCT(state, product) {
       state.product = product;
     },
-    setUsers: (state, users) => {
-      state.users = users;
-    },
-    setUser: (state, user) => {
-      state.user = user;
-    },
-    setRegStatus(state, status) {
-      state.regStatus = status;
-    },
-    setLogStatus(state, status) {
-      state.logStatus = status;
-    },
+
     setToken(state, token) {
       state.token = token;
-      Cookies.set("userToken", token, {
-        expires: 1,
-        path: "/",
-        secure: true,
-        sameSite: "None",
+    },
+
+    setUsers(state, users) {
+      state.users = users;
+    },
+
+    setSpinner(state, products) {
+      state.showSpinner = products;
+    },
+    setFilteredProducts(state, selectedCategory) { // Mutation to set filteredProducts
+      state.filteredProducts = state.products.filter((product) => {
+        return product.category === selectedCategory || selectedCategory === "all";
       });
     },
-    setUserData(state, userData) {
-      state.userData = userData;
-      if (userData && userData.userRole) {
-        state.userRole = userData.userRole;
-        localStorage.setItem("userData", JSON.stringify(userData));
-        console.log(userData, userData.userRole);
-      } else {
-        state.userData = null;
-        state.userRole = null;
-        localStorage.removeItem("userData");
-      }
     },
-    setMsg(state, msg) {
-      state.msg = msg;
-    },
-    setError(state, error) {
-      state.error = error;
-    },
-  },
+ 
+
+ 
   actions: {
 
-    getUsers: async (context) => {
-      try {
-        const res = await fetch(`${apiUrl}users`);
-        if (!res.ok) {
-          throw new Error("Failed to fetch users");
-        }
-        const users = await res.json();
-        context.commit("setUsers", users);
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      }
-    },
-
-    getUser: async (context, id) => {
-      try {
-        const res = await fetch(`${apiUrl}users/${id}`);
-        if (!res.ok) {
-          throw new Error("Failed to fetch user by ID");
-        }
-        const user = await res.json();
-
-        context.commit("setUser", user);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    
-    async getProducts(context) {
+    async fetchProducts(context) {
       try {
         let response = await fetch(`${apiUrl}products`);
-        let  results  = await response.json();
+        let { results } = await response.json();
         context.commit("SET_PRODUCTS", results);
       } catch (error) {
         alert(error.message);
       }
     },
-
-   
-    async getProducts(context, id) {
+    
+    async fetchProduct(context, id) {
       try {
         let response = await fetch(`${apiUrl}product/${id}`);
-        let result = await response.json();
+        let {result} = await response.json();
         context.commit("SET_PRODUCT", result[0]);
       } catch (error) {
         alert(error.message);
       }
     },
 
-    async register(context, payload) {
-      console.log("Reached");
+    async fetchUsers(context) {
       try {
-        const res = await axios.post(`${apiUrl}users`, payload);
-        console.log("Res: ", res.data);
-        const { msg, err, token } = res.data;
-        if (msg === "An error occured") {
-          context.commit("setError", msg);
-          context.commit("setRegStatus", "Not registered");
-          return { success: false, error: msg };
-        } else if (token) {
-          context.commit("setToken", token);
-          context.commit("setRegStatus", "Registered successfully");
-          return { success: true, token };
-        } else if (err) {
-          console.error(err);
-        }
-      } catch (e) {
-        context.commit("setError", e);
-        console.log(e);
-        context.commit("setRegStatus", "Not registered");
-        throw e;
+        let response = await fetch(`${apiUrl}users`);
+        let { results } = await response.json();
+        context.commit("setUsers", results);
+      } catch (error) {
+        alert(error.message);
       }
+    },
+    filterProducts(context, selectedCategory) {
+      const products = context.state.products;
+      const filteredProducts = products.filter((product) => {
+        return product.category === selectedCategory || selectedCategory === "all";
+      });
+      context.commit("setFilteredProducts", filteredProducts);
     },
 
-    async login(context, payload) {
-      try {
-        const res = await axios.post(`${apiUrl}users/login`, payload);
-        const { msg, err, token, userData } = res.data;
-        if (msg === "You are providing the wrong email or password") {
-          console.log(msg);
-          context.commit("setError", msg);
-          context.commit("setLogStatus", "Not logged in");
-          return { success: false, error: msg };
-        }
-        if (token) {
-          context.commit("setUser", userData);
-          context.commit("setToken", token);
-
-          context.commit("setLogStatus", res.data.message);
-          Cookies.set("userToken", token, {
-            expires: 1,
-          });
-          return { success: res.data.success, token };
-        } else if (err) {
-          context.commit("setToken", null);
-          context.commit("setError", err);
-          return { success: false, error: err };
-        } else {
-          context.commit("setError", "Unknown error during login");
-          context.commit("setLogStatus", "not logged in");
-          return { success: false, error: "Unknown error" };
-        }
-      } catch (err) {
-        if (err.resp) {
-          console.error(
-            "Server gave an error: ",
-            err.resp.status,
-            err.resp.data
-          );
-        } else if (err.req) {
-          console.error(
-            "No response from the server. Check your internet connection"
-          );
-        } else {
-          console.log("An error occured: ", err);
-        }
-        context.commit("setError", "An error occured while trying to log in");
-        context.commit("setLogStatus", "Not logged in");
-        return { success: false, error: "Network error" };
-      }
-    },
-
-    cookieCheck(context) {
-      const token = Cookies.get("userToken");
-      if (token) {
-        context.commit("setToken", token);
-      }
-    },
-    init(context) {
-      const cartData = localStorage.getItem("cart");
-      if (cartData) {
-        context.commit("setCart", JSON.parse(cartData));
-      }
-    },
-    init(context) {
-      context.dispatch("cookieCheck");
-    },
-    async logout(context) {
-      context.commit("setToken", null);
-      context.commit("setUser", null);
-      context.commit("setUserData", null);
-      Cookies.remove("userToken");
-    },
+    
   },
-  modules: {},
-});
+  modules: {
+  }
+})
