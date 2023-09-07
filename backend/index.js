72477cece018908f0325c2e0a877d8856009f972
zulaigahbenjamin <130163047+zulaigahbenjamin@users.noreply.github.com>
2023-09-07 11:36:42 +0200

    Update index.js cors

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ const port = +process.env.PORT || 3000
 
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header("Access-Control-Request-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header("Access-Control-Expose-Headers", "Authorization");
     next();
 })
